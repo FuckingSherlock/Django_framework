e28fde4c0e47c8691d8d413a97114bad5cb822e4
FuckingSherlock_linux <root@kali>
2022-08-25 07:47:11 -0400

    lesson_3 Args in url

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -8,8 +8,9 @@ app_name = MainappConfig.name
 urlpatterns = [
     path("", views.MainPageView.as_view(), name="index"),
     path("новости/", views.NewsPageView.as_view(), name="news"),
+    path("новости/<int:page>/", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
     path("курсы/", views.CoursesPageView.as_view(), name="courses_list"),
     path("контакты/", views.ContactsPageView.as_view(), name="contacts"),
     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
-    path("login/", views.LoginPageView.as_view(), name="login"),
+    path("вход/", views.LoginPageView.as_view(), name="login"),
 ]
