919531e58d1b4bc23e54a862591a69dc7d75dfd4
FuckingSherlock_linux <root@kali>
2022-08-25 05:17:30 -0400

    lesson_3 Tag with

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,9 +7,9 @@ app_name = MainappConfig.name
 
 urlpatterns = [
     path("", views.MainPageView.as_view(), name="index"),
-    path("news/", views.NewsPageView.as_view(), name="news"),
-    path("courses_list/", views.CoursesPageView.as_view(), name="courses_list"),
-    path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
+    path("новости/", views.NewsPageView.as_view(), name="news"),
+    path("курсы/", views.CoursesPageView.as_view(), name="courses_list"),
+    path("контакты/", views.ContactsPageView.as_view(), name="contacts"),
     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
     path("login/", views.LoginPageView.as_view(), name="login"),
 ]
